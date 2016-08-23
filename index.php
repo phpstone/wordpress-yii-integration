e3bac3bc37ff7c9e86e8de3400f8b2f00bb57bae
freperl <freperl@mac-meng.lan>
2016-08-23 09:41:44 +0800

    First file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+define('WP_USE_THEMES', true);
+$wp_did_header = true;
+require_once('blog/wp-load.php');
+ 
+require_once(dirname(__FILE__) . '/protected/components/ExceptionHandler.php');
+$router = new ExceptionHandler();
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../../../framework/yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
